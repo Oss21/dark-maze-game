768e12edbeb8b7c2e6930fa6e3dbe21f0a8ca6d1
EliasE12 <simply753@gmail.com>
2019-11-26 10:15:17 -0500

    Se  han agregado los atributos de la interface a los controladores

--- src/controller/MazeGameController.java
+++ src/controller/MazeGameController.java
@@ -3,13 +3,48 @@ package controller;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 
 public class MazeGameController implements Initializable {
+	
+	    @FXML
+	    private Button btMatriz;
 
+	    @FXML
+	    private Button btLista;
+
+	    @FXML
+	    private Button btDarSolucion;
+
+	    @FXML
+	    private Button btIrAPuntoDeLuz;
+
+	    @FXML
+	    private Button btUsarLinterna;
+
+	    @FXML
+	    private Button btGuardarPartida;
+
+	    @FXML
+	    private Button btAtras;
+
+	    @FXML
+	    private Pane pnMaze;
+	
+	    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+    
+    
+    
+    
+    
+    
 }
 
 
--- src/controller/StartWindowsController.java
+++ src/controller/StartWindowsController.java
@@ -3,10 +3,27 @@ package controller;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class StartWindowsController implements Initializable {
 
 
+	 @FXML
+	    private Button btNuevaPartida;
+
+	    @FXML
+	    private Button btCargarPartida;
+
+	    @FXML
+	    private Button btSalir;
+	
+	
+	
+	    
+	    
+	    
+	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
