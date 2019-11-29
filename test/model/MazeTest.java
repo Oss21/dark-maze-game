3801f1bfc852f2c2de1e604db461a7df05353020
Osss21 <osvan202@gmail.com>
2019-11-29 04:49:48 -0500

    Se implementaron las pruebas de la clase methodGraph, vertex y Edge

---
+++ test/model/MazeTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MazeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
